2045801e199ea9cba091c1e0f930b42b3fcf8ce0
Alon Albalak <alon_albalak@ucsb.edu>
2023-06-06 14:21:21 -0700

    update example training run and config names

--- example_train_run.sh
+++ example_train_run.sh
@@ -1,2 +1,5 @@
+CONFIGS="alon_configs/data/pile_v2.yml alon_configs/models/160.yml alon_configs/init.yml alon_configs/optimizer.yml alon_configs/parallelism.yml"
+echo "Running with configs: ${CONFIGS}"
+
 RUN_NAME="current_run"
-python3 deepy.py train.py configs/alon_config_small.yml 2>&1 | tee outputs/${RUN_NAME}.log
+python3 deepy.py train.py ${CONFIGS} 2>&1 | tee outputs/${RUN_NAME}.log

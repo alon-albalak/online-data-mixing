92f5e04cbc260de601c176d5860c6e9b8239b550
Alon <alonalbalak@gmail.com>
2023-06-15 15:10:56 -0700

    add scripts for training all models with specific domain weights

---
+++ scripts/train_doremi_1B.sh
@@ -0,0 +1,20 @@
+# GENERAL CONFIGS THAT WILL BE USED FOR ALL RUNS
+CONFIGS="alon_configs/data/pile.yml alon_configs/init.yml alon_configs/optimizer.yml alon_configs/train_data_weights/doremi_1B.yml"
+# Doesn't include alon_configs/eval_tasks.yml or  alon_configs/parallelism.yml
+
+# RUN SPECIFIC CONFIGS
+CONFIGS_160M="alon_configs/models/160m.yml alon_configs/run_specific/160m_doremi_1B.yml"
+CONFIGS_410M="alon_configs/models/410m.yml alon_configs/run_specific/410m_doremi_1B.yml"
+CONFIGS_1B="alon_configs/models/1B.yml alon_configs/run_specific/1B_doremi_1B.yml"
+
+echo "Running with configs: ${CONFIGS} ${CONFIGS_160M}"
+RUN_NAME="160m_doremi_1B"
+python3 deepy.py train.py ${CONFIGS} ${CONFIGS_160M} 2>&1 | tee outputs/${RUN_NAME}.log
+
+echo "Running with configs: ${CONFIGS} ${CONFIGS_410M}"
+RUN_NAME="410m_doremi_1B"
+python3 deepy.py train.py ${CONFIGS} ${CONFIGS_410M} 2>&1 | tee outputs/${RUN_NAME}.log
+
+echo "Running with configs: ${CONFIGS} ${CONFIGS_1B}"
+RUN_NAME="1B_doremi_1B"
+python3 deepy.py train.py ${CONFIGS} ${CONFIGS_1B} 2>&1 | tee outputs/${RUN_NAME}.log
---
+++ scripts/train_original_pile.sh
@@ -0,0 +1,20 @@
+# GENERAL CONFIGS THAT WILL BE USED FOR ALL RUNS
+CONFIGS="alon_configs/data/pile.yml alon_configs/init.yml alon_configs/optimizer.yml alon_configs/train_data_weights/original_pile.yml"
+# Doesn't include alon_configs/eval_tasks.yml or  alon_configs/parallelism.yml
+
+# RUN SPECIFIC CONFIGS
+CONFIGS_160M="alon_configs/models/160m.yml alon_configs/run_specific/160m_original.yml"
+CONFIGS_410M="alon_configs/models/410m.yml alon_configs/run_specific/410m_original.yml"
+CONFIGS_1B="alon_configs/models/1B.yml alon_configs/run_specific/1B_original.yml"
+
+echo "Running with configs: ${CONFIGS} ${CONFIGS_160M}"
+RUN_NAME="160m_original"
+python3 deepy.py train.py ${CONFIGS} ${CONFIGS_160M} 2>&1 | tee outputs/${RUN_NAME}.log
+
+echo "Running with configs: ${CONFIGS} ${CONFIGS_410M}"
+RUN_NAME="410m_original"
+python3 deepy.py train.py ${CONFIGS} ${CONFIGS_410M} 2>&1 | tee outputs/${RUN_NAME}.log
+
+echo "Running with configs: ${CONFIGS} ${CONFIGS_1B}"
+RUN_NAME="1B_original"
+python3 deepy.py train.py ${CONFIGS} ${CONFIGS_1B} 2>&1 | tee outputs/${RUN_NAME}.log

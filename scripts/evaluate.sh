07c26509c2854cd7fa65758a9f62a3b461f82ba8
Alon Albalak <alonalbalak@gmail.com>
2023-07-05 11:29:42 -0700

    add tool to create eval-specific config, remove trailing commas from yml/jsons

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -9,6 +9,10 @@ EVAL_TASKS="lambada_openai piqa winogrande arc_easy sciq wikitext openbookqa"
 #         "load": "outputs/160m_doremi_280_seed42",
 #         "eval_results_prefix": "outputs/160m_doremi_280_seed42",
 # CONFIGS="outputs/160m_doremi_280_seed42/global_step100000/configs/160m_doremi_280_seed42.yml alon_configs/models/eval_160m_1gpu.yml"
+python3 tools/create_eval_config.py --config_path ${MODEL_CONFIG}
+
+# EVAL_MODEL_CONFIG is MODEL_CONFIG with .yml replaced by _eval.yml
+EVAL_MODEL_CONFIG=${MODEL_CONFIG%.yml}_eval.yml
 
 # EVAL_CONFIG should be in the configs folder. See alon_configs/models/eval_160m_1gpu.yml for an example
-python ./deepy.py evaluate.py ${MODEL_CONFIG} ${EVAL_CONFIG} --eval_tasks ${EVAL_TASKS}
+python ./deepy.py evaluate.py ${EVAL_MODEL_CONFIG} ${EVAL_CONFIG} --eval_tasks ${EVAL_TASKS}
---
+++ tools/create_eval_config.py
@@ -0,0 +1,34 @@
+# Simple utility that takes an existing yml config file and adds evaluation specific parameters.
+
+import argparse
+import json
+import os
+
+def get_step_from_path(path):
+    top_dir = path.split("/")[-1]
+    if "global_step" in top_dir:
+        return int(top_dir.split("global_step")[-1])
+    else:
+        return get_step_from_path("/".join(path.split("/")[:-1])) 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--config_path", type=str, required=True, help="Path to the config file to modify.")
+
+args = parser.parse_args()
+model_step = get_step_from_path(args.config_path)
+
+with open(args.config_path, "r") as f:
+    config = json.load(f)
+
+if not hasattr(config, "seed"):
+    config["seed"] = 1234
+
+config["wandb_run_name"] = f"seed{config['seed']}_eval"
+
+eval_results_prefix = os.path.join(config["save"], f"step{model_step}")
+config['load'] = config['save']
+config['eval_results_prefix'] = eval_results_prefix
+
+save_path = args.config_path.replace(".yml", "_eval.yml")
+with open(save_path, "w") as f:
+    json.dump(config, f, indent=2)

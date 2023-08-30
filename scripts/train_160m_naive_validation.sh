a11caa7c381254a50932a4d22c8f35f7f81e0d9b
Alon Albalak <alonalbalak@gmail.com>
2023-08-30 10:31:44 -0700

    add 1B exp3 and ema experiments

--- scripts/train_160m_naive_validation.sh
+++ scripts/train_160m_naive_validation.sh
@@ -7,13 +7,14 @@ DATA_SAMPLING_METHOD="naive_validation"
 DATA_SAMPLING_WARMUP_STEPS="2000"
 DATA_SAMPLING_UPDATE_FREQUENCY="10"
 MIXED_BATCHES=true
+VALIDATION_BASED_REWARD=true
 
 SEEDS=(1234 42 100 222)
 
 # RUN SPECIFIC CONFIGS
 for SEED in ${SEEDS[@]}; do
     RUN_NAME="${WANDB_GROUP}_seed${SEED}"
-    ARGS="--seed ${SEED} --save outputs/${RUN_NAME} --wandb_group ${WANDB_GROUP} --wandb_run_name seed${SEED} --data_sampling_method ${DATA_SAMPLING_METHOD} --data_sampling_warmup_steps ${DATA_SAMPLING_WARMUP_STEPS} --data_sampling_update_frequency ${DATA_SAMPLING_UPDATE_FREQUENCY} --mixed_batches ${MIXED_BATCHES}"
+    ARGS="--seed ${SEED} --save outputs/${RUN_NAME} --wandb_group ${WANDB_GROUP} --wandb_run_name seed${SEED} --data_sampling_method ${DATA_SAMPLING_METHOD} --data_sampling_warmup_steps ${DATA_SAMPLING_WARMUP_STEPS} --data_sampling_update_frequency ${DATA_SAMPLING_UPDATE_FREQUENCY} --mixed_batches ${MIXED_BATCHES} --validation_based_reward ${VALIDATION_BASED_REWARD}"
     python3 tools/create_run_specific_config.py ${ARGS}
     RUN_SPECIFIC_CONFIG="alon_configs/run_specific/${RUN_NAME}.yml"
     echo "Running with configs: ${CONFIGS} ${RUN_SPECIFIC_CONFIG}"

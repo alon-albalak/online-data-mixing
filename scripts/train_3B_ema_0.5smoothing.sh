e8f4cb6172f39c9d6623da3b6c440bd6a589e75f
Alon Albalak <alonalbalak@gmail.com>
2023-10-04 11:20:00 -0700

    add 3B scripts

---
+++ scripts/train_3B_ema_0.5smoothing.sh
@@ -0,0 +1,31 @@
+# GENERAL CONFIGS THAT WILL BE USED FOR ALL RUNS
+CONFIGS="alon_configs/data/pile.yml alon_configs/init.yml alon_configs/models/3B.yml alon_configs/eval_tasks.yml alon_configs/train_data_weights/original_pile.yml"
+# Doesn't include alon_configs/parallelism.yml
+
+WANDB_GROUP="3B_ods_smoothed_mean_mixed_minibatches_original_weights_init_05smoothing"
+DATA_SAMPLING_METHOD="smoothed_mean"
+DATA_SAMPLING_WARMUP_STEPS="2000"
+DATA_SAMPLING_UPDATE_FREQUENCY="1"
+MIXED_MINIBATCHES=true
+SMOOTHING_FACTOR="0.5"
+
+# SEEDS=(1234 42 100 222)
+SEEDS=(42)
+
+# RUN SPECIFIC CONFIGS
+for SEED in ${SEEDS[@]}; do
+    RUN_NAME="${WANDB_GROUP}_seed${SEED}"
+    ARGS="--seed ${SEED} --save outputs/${RUN_NAME} --wandb_group ${WANDB_GROUP} --wandb_run_name seed${SEED} --data_sampling_method ${DATA_SAMPLING_METHOD} --data_sampling_warmup_steps ${DATA_SAMPLING_WARMUP_STEPS} --data_sampling_update_frequency ${DATA_SAMPLING_UPDATE_FREQUENCY} --mixed_minibatches ${MIXED_MINIBATCHES} --data_sampling_smoothing_factor ${SMOOTHING_FACTOR}"
+    python3 tools/create_run_specific_config.py ${ARGS}
+    RUN_SPECIFIC_CONFIG="alon_configs/run_specific/${RUN_NAME}.yml"
+    echo "Running with configs: ${CONFIGS} ${RUN_SPECIFIC_CONFIG}"
+    python3 deepy.py train.py ${CONFIGS} ${RUN_SPECIFIC_CONFIG} 2>&1 | tee outputs/${RUN_NAME}.log
+    
+    # # evaluate 0-shot
+    # bash scripts/evaluate.sh outputs/${RUN_NAME}/global_step100000/configs/${RUN_NAME}.yml alon_configs/models/eval_1B_1gpu.yml 2>&1 | tee outputs/${RUN_NAME}_eval.log &
+    # # evaluate 1-shot through 5-shot
+    # for i in {1..5}; do
+    #     bash scripts/evaluate.sh outputs/${RUN_NAME}/global_step100000/configs/${RUN_NAME}.yml alon_configs/models/eval_1B_1gpu_local${i}.yml ${i} 2>&1 | tee outputs/${RUN_NAME}_${i}shot_eval.log &
+    # done
+    # wait < <(jobs -p)
+done

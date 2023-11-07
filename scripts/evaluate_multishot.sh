3e5d10a978c7dc524d551d3d20544e196c440b77
Alon <alonalbalak@gmail.com>
2023-11-06 19:20:20 -0800

    make multishot evaluation more flexible

--- scripts/evaluate_multishot.sh
+++ scripts/evaluate_multishot.sh
@@ -1,10 +1,15 @@
 RUN_NAME=$1
-STEP=$2
+MODEL_CONFIG_EVAL=$2
+STEP=$3
+
+# MODEL_CONFIG_EVAL is the file name in the alon_configs/models/ directory
+# e.g. alon_configs/models/eval_1B_1gpu.yml
+# OR alon_configs/models/eval_3B_seqlen2048_1gpu.yml
 
 # evaluate 0-shot
-bash scripts/evaluate.sh outputs/${RUN_NAME}/global_step${STEP}/configs/${RUN_NAME}.yml alon_configs/models/eval_3B_1gpu.yml 0 2>&1 | tee outputs/${RUN_NAME}_${STEP}_eval.log &
+bash scripts/evaluate.sh outputs/${RUN_NAME}/global_step${STEP}/configs/${RUN_NAME}.yml alon_configs/models/${MODEL_CONFIG_EVAL}.yml 0 2>&1 | tee outputs/${RUN_NAME}_${STEP}_eval.log &
 # evaluate 1-shot through 5-shot
 for i in {1..5}; do
-    bash scripts/evaluate.sh outputs/${RUN_NAME}/global_step${STEP}/configs/${RUN_NAME}.yml alon_configs/models/eval_3B_1gpu.yml ${i} ${i} 2>&1 | tee outputs/${RUN_NAME}_${STEP}_${i}shot_eval.log &
+    bash scripts/evaluate.sh outputs/${RUN_NAME}/global_step${STEP}/configs/${RUN_NAME}.yml alon_configs/models/${MODEL_CONFIG_EVAL}.yml ${i} ${i} 2>&1 | tee outputs/${RUN_NAME}_${STEP}_${i}shot_eval.log &
 done
 wait < <(jobs -p)
